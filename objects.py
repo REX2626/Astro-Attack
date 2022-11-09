434a23d85b7e0f2114e27af0a3602773453928db
Rex Attwood <rexattwood26@gmail.com>
2022-11-09 15:39:36 +0000

    Updated pos and vec with new vector class

--- objects.py
+++ objects.py
@@ -32,6 +32,16 @@ class Vector():
         else:
             return Vector(self.x + arg, self.y + arg)
 
+    def __mul__(self, arg):
+
+        # Multiplying Vectors
+        if type(arg) == Vector:
+            return Vector(self.x * arg.x, self.y * arg.y)
+
+        # Multiplying Vector with Scalar
+        else:
+            return Vector(self.x * arg, self.y * arg)
+
     def __repr__(self):
         return str((self.x, self.y))
 
@@ -39,13 +49,16 @@ class Vector():
         self.x = round(self.x)
         self.y = round(self.y)
 
+    def magnitude(self):
+        return (self.x**2 + self.y**2) ** 0.5
+
 
 
 class Object():
     def __init__(self, position, width, height, image) -> None:
         
         # Make position a vector
-        if type(self.position) != Vector:
+        if type(position) != Vector:
             self.position = Vector(position[0], position[1])
         else:
             self.position = position
@@ -62,53 +75,37 @@ class Object():
 class MoveableObject(Object):
     def __init__(self, position, velocity, width, height, image) -> None:
         super().__init__(position, width, height, image)
-        self.vx = vx
-        self.vy = vy
 
-    def update_posx(self, delta_time):
-        self.position.x += self.vx * delta_time
-    
-    def update_posy(self, delta_time):
-        self.position.y += self.vy * delta_time
+        # Make velocity a vector
+        if type(velocity) != Vector:
+            self.velocity = Vector(velocity[0], velocity[1])
+        else:
+            self.velocity = velocity
 
     def update_pos(self, delta_time):
-        self.update_posx(delta_time)
-        self.update_posy(delta_time)
         self.position += self.velocity * delta_time
 
-    def change_vel(self, ax, ay):
-        self.vx += ax
-        self.vy += ay
-
-    def get_mag(self, a, b):
-        return (a**2 + b**2) ** 0.5
-
-    def vel_mag(self):
-        return self.get_mag(self.vx, self.vy)
-
     def set_vel_mag(self, mag):
-        k = mag / self.vel_mag()
-        self.vx *= k
-        self.vy *= k
+        k = mag / self.velocity.magnitude()
+        self.velocity *= k
 
     def clamp_vel(self, max_speed):
-        if self.vel_mag() > max_speed:
+        if self.velocity.magnitude() > max_speed:
             self.set_vel_mag(max_speed)
 
 
 
 class Ship(MoveableObject):
-    def __init__(self, x, y, vx, vy, width, height, image) -> None:
-        super().__init__(x, y, vx, vy, width, height, image)
+    def __init__(self, position, velocity, width, height, image) -> None:
+        super().__init__(position, velocity, width, height, image)
 
 
 
 class Player_Ship(Ship):
-    def __init__(self, x, y, vx, vy, width, height, max_speed, image) -> None:
-        super().__init__(x, y, vx, vy, width, height, image)
+    def __init__(self, position, velocity, width, height, max_speed, image) -> None:
+        super().__init__(position, velocity, width, height, image)
         self.max_speed = max_speed
 
-    def change_vel(self, ax, ay):
-        self.vx += ax
-        self.vy += ay
+    def change_vel(self, acceleration):
+        self.velocity + acceleration
         self.clamp_vel(self.max_speed)
