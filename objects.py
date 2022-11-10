71685872ea73903bbd740720f319004705cfd21b
Rex2626 <rexattwood26@gmail.com>
2022-11-10 21:51:24 +0000

    Refined Vector Class, finished intertial dampening

--- objects.py
+++ objects.py
@@ -31,6 +31,16 @@ class Vector():
         # Adding Vector to Scalar
         else:
             return Vector(self.x + arg, self.y + arg)
+
+    def __truediv__(self, arg):
+
+        # Dividing Vectors
+        if type(arg) == Vector:
+            return Vector(self.x / arg.x, self.y / arg.y)
+        
+        # Dividing Vector by Scalar
+        else:
+            return Vector(self.x / arg, self.y / arg)
     
     def __sub__(self, arg):
 
@@ -68,8 +78,25 @@ class Vector():
     def __round__(self):
         return Vector(round(self.x), round(self.y))
 
+    def clamp(self, maximum):
+        if self.magnitude() > maximum:
+            self.set_magnitude(maximum)
+
+    def get_clamp(self, maximum):
+        if self.magnitude() > maximum:
+            # Set magnitude to maximum
+            return self * maximum / self.magnitude()
+        return self
+
     def magnitude(self):
         return (self.x**2 + self.y**2) ** 0.5
+
+    def set_magnitude(self, magnitude):
+        # cringe way of updating self
+        # can't do "self = new_vector" as self is just a variable
+        new_vector = self * magnitude / self.magnitude()
+        self.x = new_vector.x
+        self.y = new_vector.y
     
     def to_tuple(self):
         return (self.x, self.y)
@@ -103,46 +130,36 @@ class MoveableObject(Object):
         if type(velocity) != Vector:
             self.velocity = Vector(velocity[0], velocity[1])
         else:
-            self.velocity = velocity
+            self.velocity: Vector = velocity
 
     def update_pos(self, delta_time):
         self.position += self.velocity * delta_time
 
-    def set_vel_mag(self, mag):
-        k = mag / self.velocity.magnitude()
-        self.velocity *= k
-
-    def clamp_vel(self, max_speed):
-        if self.velocity.magnitude() > max_speed:
-            self.set_vel_mag(max_speed)
-
 
 
 class Ship(MoveableObject):
-    def __init__(self, position, velocity, width, height, image) -> None:
+    def __init__(self, position: Vector, velocity: Vector, width, height, image) -> None:
         super().__init__(position, velocity, width, height, image)
 
 
 
 class Player_Ship(Ship):
-    def __init__(self, position, velocity, width, height, max_speed, image) -> None:
+    def __init__(self, position: Vector, velocity: Vector, width, height, max_speed, image) -> None:
         super().__init__(position, velocity, width, height, image)
         self.max_speed = max_speed
 
-    def change_vel(self, acceleration):
+    def change_vel(self, acceleration: Vector):
         self.velocity += acceleration
-        self.clamp_vel(self.max_speed)
+        self.velocity.clamp(self.max_speed)
     
     def update_pos(self, delta_time):
         super().update_pos(delta_time)
 
         # Inertial Dampening
-        if self.velocity.x > 0:
-            self.velocity.x -= 200 * delta_time
-        elif self.velocity.x < 0:
-            self.velocity.x += 200 * delta_time
-        if self.velocity.y > 0:
-            self.velocity.y -= 200 * delta_time
-        elif self.velocity.y < 0:
-            self.velocity.y += 200 * delta_time
-        #self.velocity -= self.velocity * delta_time
+        """
+        -> velocity is added with the inverse velocity, making velocity 0
+        -> but inverse velocity is clamped so it doesn't go to 0 velocity instantly
+        -> 200 is a constant
+        -> the bigger the constant, the faster the dampening
+        """
+        self.velocity -= self.velocity.get_clamp(200 * delta_time)
